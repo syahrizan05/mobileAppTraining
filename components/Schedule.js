64c549163c0afac5e7e1c0cc71a65d857ba5e1ee
syahrizan <syahrizan@rbtsb.com>
2021-07-14 10:06:13 +0800

    customize schedule

--- components/Schedule.js
+++ components/Schedule.js
@@ -4,8 +4,33 @@ import { Ionicons } from "@expo/vector-icons";
 
 const Schedule = () => {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      
+    <View style={{ flex: 1 }}>
+      <ScheduleRow day={"Monday"} morning={"Physics"} afternoon={"Language"} />
+      <ScheduleRow
+        day={"Tuesday"}
+        morning={"Mathematics"}
+        afternoon={"Communication"}
+      />
+      <ScheduleRow />
+      <ScheduleRow />
+      <ScheduleRow />
+      <ScheduleRow />
+    </View>
+  );
+};
+
+const ScheduleRow = ({ day, morning, afternoon }) => {
+  return (
+    <View style={{ flex: 1, flexDirection: "row",margin:10 }}>
+      <View style={{ flex: 1 }}>
+        <Text>{day}</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text>{morning}</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text>{afternoon}</Text>
+      </View>
     </View>
   );
 };
