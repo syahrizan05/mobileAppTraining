ae56775490b248bef4840b9bdfd186ce6656ecf9
syahrizan <syahrizan@rbtsb.com>
2021-07-14 08:31:45 +0800

    modified header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Header = () => {
   return (
     <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
-      <View style={{ flex: 1, backgroundColor: "darkturquoise" }}></View>
-      <View style={{ flex: 1, backgroundColor: "purple" }}></View>
+      <View style={{ flex: 1 }}></View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 25, color: "red" }}>HOME</Text>
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Ionicons name={"home"} size={32} color={"purple"} />
+      </View>
     </View>
   );
 };
